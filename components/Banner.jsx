1606f3ad13f4a92a63b1e5d98b99f09687133a14
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-06-14 19:42:58 -0300

    feat: adicionando icones do componente de banner principal

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,7 +1,8 @@
-// import Image from 'next/image'
 import Image from 'next/image'
 import styles from '../styles/Banner.module.css'
 import augustoImg from '../images/augusto.jpg';
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Typewriter from 'typewriter-effect';
 
 export default function Banner() {
@@ -11,19 +12,26 @@ export default function Banner() {
                 <p className={ styles.hello }>Olá!</p>
                 <h2 className={ styles.titleWhite }>Eu sou o Augusto</h2>
                 <h2  className={ styles.titleColor }>Barbosa</h2>
-                {/* <span>TEXTO MUDANDO</span> */}
                 <Typewriter
                     options={{
-                        strings: ['Desenvolvedor Full stack', 'Bacharel em Sistemas de Informação'],
+                        strings: ['Desenvolvedor Full stack', 'Bacharel em Sistemas de Informação', 'Estudante de UX design'],
                         autoStart: true,
                         loop: true,
-                        delay: 50,
-                      }}
+                        delay: 80,
+                        wrapperClassName: styles.typeWriterText,
+                    }}
                 />
-                <button>Curriculum </button>
+                <button className={ styles.btnCurriculum }>
+                    {/* <i src={ augustoImg } /> &nbsp; Curriculum */}
+                    <FontAwesomeIcon className={ styles.downIcon } icon={ faDownload  } /> &nbsp;
+                    Curriculum
+                </button>
             </div>
             <div className={ styles.imageBannerArea }>
-                <Image src={ augustoImg } alt='augusto-foto' className={ styles.augustoFoto }/>
+                {/* <div className={ styles.animateArea }> */}
+                    <div className={ styles.animateCircle } />
+                    <Image src={ augustoImg } alt='augusto-foto' className={ styles.augustoFoto }/>   
+                {/* </div>                 */}
             </div>
         </div>
     )
