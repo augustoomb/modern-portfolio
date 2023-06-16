6f6a10f476f3465809ffff35e37dc3e6362f8273
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-06-16 18:08:32 -0300

    feat: arquivo contem array de objetos com minhas skills

---
+++ data/dataSkills.js
@@ -0,0 +1,33 @@
+import reactLogo from '../images/react-icon.svg';
+import nodejs from '../images/nodejs-icon.svg';
+import mysql from '../images/mysql-icon.svg';
+import nextjs from '../images/nextjs-icon.svg';
+import js from '../images/js-icon.svg';
+import ts from '../images/typescript-icon.svg';
+import mongo from '../images/mongodb.svg';
+import figma from '../images/figma-icon.svg';
+import git from '../images/git-icon.svg';
+import python from '../images/python-icon.svg';
+import css from '../images/css-icon.svg';
+import html from '../images/html-icon.svg';
+import docker from '../images/docker-icon.svg';
+import swagger from '../images/swagger-icon.svg';
+
+const arrSkills = [
+    { "logo": html, "name": 'HTML' },
+    { "logo": css, "name": 'CSS' },
+    { "logo": reactLogo, "name": 'React' },
+    { "logo": nodejs, "name": 'Node.js' },
+    { "logo": mysql, "name": 'MySQL' },
+    { "logo": nextjs, "name": 'Next.js' },
+    { "logo": js, "name": 'JavaScript' },
+    { "logo": ts, "name": 'TypeScript' },
+    { "logo": mongo, "name": 'MongoDB' },
+    { "logo": figma, "name": 'Figma' },
+    { "logo": git, "name": 'Git' },
+    { "logo": python, "name": 'Python' },
+    { "logo": docker, "name": 'Docker' },
+    { "logo": swagger, "name": 'Swagger' },
+] 
+
+export default arrSkills;
