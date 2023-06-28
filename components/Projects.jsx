7272a14ec3d7692c992966475ae08763cbcdcccf
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-06-28 19:41:04 -0300

    feat: iterando sob a lista de projetos

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,22 @@
+import styles from '../styles/Projects.module.css';
+import arrProjects from '../data/dataProjects';
+import Project from './Project';
+
 export default function Projects() {
     return (
-        <div className="styles.projects">
-            <h2>Projetos</h2>
+        <div className={ styles.projectsArea }>
+            <h2 className={ styles.titleProjects }>Projetos</h2>
+            <div className={ styles.projectsData }>
+                {
+                    arrProjects.map((proj, index) => (<Project
+                        key={ index }
+                        cover={ proj.cover }
+                        title={ proj.title }
+                        text={ proj.text }                     
+                        tech={ proj.technologies }                     
+                    />))
+                }
+            </div>
         </div>
     )
 }
