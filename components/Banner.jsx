3091a6e0fc91b3cd615fa6926101e996525130a1
Augusto Barbosa <augustobarbosa1988@gmail.com>
2023-07-03 18:11:06 -0300

    fix: lib react wrap balancer foi desabilitada

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -4,6 +4,8 @@ import augustoImg from '../images/augusto.jpg';
 import { faDownload } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Typewriter from 'typewriter-effect';
+import { HeroPattern } from './HeroPattern'; // animação traços no fundo da tela
+// import Balance from 'react-wrap-balancer'; // mantem o wrap balanceado
 
 export default function Banner() {
     return (
@@ -12,20 +14,22 @@ export default function Banner() {
                 <p className={ styles.hello }>Olá!</p>
                 <h2 className={ styles.titleWhite }>Eu sou o Augusto</h2>
                 <h2  className={ styles.titleColor }>Barbosa</h2>
-                <Typewriter
-                    options={{
-                        strings: ['Desenvolvedor Full stack', 'Bacharel em Sistemas de Informação', 'Estudante de UX design'],
-                        autoStart: true,
-                        loop: true,
-                        delay: 80,
-                        wrapperClassName: styles.typeWriterText,
-                    }}
-                />
+                {/* <Balance> */}
+                    <Typewriter
+                        options={{
+                            strings: ['Desenvolvedor Full stack', 'Bacharel em Sistemas de Informação', 'Estudante de UX design'],
+                            autoStart: true,
+                            loop: true,
+                            delay: 80,
+                            wrapperClassName: styles.typeWriterText,
+                        }}
+                    />
+                {/* </Balance> */}
                 <button className={ styles.btnCurriculum }>
                     {/* <i src={ augustoImg } /> &nbsp; Curriculum */}
                     <FontAwesomeIcon className={ styles.downIcon } icon={ faDownload  } /> &nbsp;
                     Curriculum
-                </button>
+                </button>                
             </div>
             <div className={ styles.imageBannerArea }>
                 {/* <div className={ styles.animateArea }> */}
@@ -33,6 +37,7 @@ export default function Banner() {
                     <Image src={ augustoImg } alt='augusto-foto' className={ styles.augustoFoto }/>   
                 {/* </div>                 */}
             </div>
+            <HeroPattern />
         </div>
     )
 }
